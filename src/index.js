17d93c33fe17fd58af38b984efe661c211d46904
Joshua Semana <jsemana054@gmail.com>
2022-03-03 17:57:10 -0700

    added changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import createPage from './pages/interface.js';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 document.addEventListener('DOMContentLoaded', createPage);
