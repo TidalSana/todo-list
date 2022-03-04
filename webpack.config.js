8e2cd6c9ccd9e00559e84438f5c4d7ef01c7d4ff
Joshua Semana <jsemana054@gmail.com>
2022-03-04 12:05:19 -0700

    more font/icon tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,6 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
-      // {
-      //   test: /\.(woff|woff2|eot|ttf|otf)$/,
-      //   loader: "file-loader",
-      //   options: {
-      //     outputPath: "../icons",
-      //   }
-      // } 
     ],
   },
 };
